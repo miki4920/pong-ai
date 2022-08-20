f3e60e315fccacd6193efdf981ff7f86e9ca7320
Snowystar122 <43116592+Snowystar122@users.noreply.github.com>
2022-08-20 16:24:12 +0100

    Created a basic pong game

---
+++ game.py
@@ -0,0 +1,87 @@
+import pygame
+
+from pygame import display, Surface
+from config import KeyBinds, Config
+
+pygame.init()
+game_display = display.set_mode((Config.SCREEN_WIDTH, Config.SCREEN_HEIGHT), pygame.RESIZABLE)
+vec = pygame.math.Vector2
+frames_per_second = pygame.time.Clock()
+
+
+class GameObject:
+    def __init__(self, positions, size, colour):
+        self.surface = Surface(vec(size))
+        self.surface.fill(colour)
+        self.rectangle = self.surface.get_rect()
+        self.rectangle.center = vec(positions)
+
+        self.velocity = vec(5, 0)
+        self.acceleration = vec(0, 0)
+
+    def draw(self):
+        return self.surface, self.rectangle
+
+
+class Ball(GameObject):
+    def __init__(self, positions, size=Config.BALL_SIZE, colour=Config.BALL_COLOUR):
+        super().__init__(positions, size, colour)
+
+
+class Player(GameObject):
+    def __init__(self, positions, size=Config.PLAYER_SIZE, colour=Config.PLAYER_COLOUR):
+        super().__init__(positions, size, colour)
+        self.dead = False
+        self.score = 0
+
+
+class Environment:
+    def __init__(self):
+        self.players = [Player((0, Config.SCREEN_HEIGHT//2 - 10)), Player((Config.SCREEN_WIDTH, Config.SCREEN_HEIGHT//2))]
+        self.ball = Ball((Config.SCREEN_WIDTH//2, Config.SCREEN_HEIGHT//2))
+
+    def account_for_collision(self):
+        for player in self.players:
+            if player.rectangle.colliderect(self.ball.rectangle):
+                player.score += 1
+                return (self.ball.rectangle.centery - player.rectangle.centery) / (Config.PLAYER_SIZE[1]//2)
+
+    def get_keys(self):
+        keys = pygame.key.get_pressed()
+        if keys[KeyBinds.UP_P1] or keys[KeyBinds.DOWN_P1]:
+            self.update_player(self.players[0], [keys[KeyBinds.UP_P1], keys[KeyBinds.DOWN_P1]])
+        if keys[KeyBinds.UP_P2] or keys[KeyBinds.DOWN_P2]:
+            self.update_player(self.players[1], [keys[KeyBinds.UP_P2], keys[KeyBinds.DOWN_P2]])
+
+    @staticmethod
+    def update_player(player, keys):
+        if keys[0] >= 0.5:
+            player.rectangle.centery -= Config.PLAYER_SPEED
+        elif keys[1] >= 0.5:
+            player.rectangle.centery += Config.PLAYER_SPEED
+
+    def update_ball(self):
+        collision = self.account_for_collision()
+        if collision is not None:
+            self.ball.velocity.x = -self.ball.velocity.x
+            self.ball.velocity.y += collision * 10
+        self.ball.rectangle.center += self.ball.velocity
+        if self.ball.rectangle.bottom >= Config.SCREEN_HEIGHT or self.ball.rectangle.top <= 0:
+            self.ball.velocity.y = -self.ball.velocity.y
+
+    def render_environment(self):
+        game_display.fill((0, 0, 0))
+        for player in self.players:
+            game_display.blit(*player.draw())
+        game_display.blit(*self.ball.draw())
+        display.update()
+
+
+if __name__ == "__main__":
+    environment = Environment()
+    while True:
+        environment.update_ball()
+        environment.get_keys()
+        environment.render_environment()
+        pygame.event.pump()
+        frames_per_second.tick(60)
