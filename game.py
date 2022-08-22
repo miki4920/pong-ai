f13ccde42804bde99a6d153e00cf4d9ffb4bcaba
miki4920 <miki4920@gmail.com>
2022-08-22 23:37:52 +0100

    Added ball colliding with top and bottom edges of the screen

--- game.py
+++ game.py
@@ -16,7 +16,7 @@ class GameObject:
         self.rectangle = self.surface.get_rect()
         self.rectangle.center = vec(positions)
 
-        self.velocity = vec(5, 0)
+        self.velocity = vec(0, 0)
         self.acceleration = vec(0, 0)
 
     def draw(self):
@@ -24,8 +24,9 @@ class GameObject:
 
 
 class Ball(GameObject):
-    def __init__(self, positions, size=Config.BALL_SIZE, colour=Config.BALL_COLOUR):
+    def __init__(self, positions, size=Config.BALL_SIZE, colour=Config.BALL_COLOUR, speed=Config.BALL_SPEED):
         super().__init__(positions, size, colour)
+        self.velocity.x = speed
 
 
 class Player(GameObject):
@@ -37,7 +38,7 @@ class Player(GameObject):
 
 class Environment:
     def __init__(self):
-        self.players = [Player((0, Config.SCREEN_HEIGHT // 2 - 10)),
+        self.players = [Player((0, Config.SCREEN_HEIGHT // 2)),
                         Player((Config.SCREEN_WIDTH, Config.SCREEN_HEIGHT // 2))]
         self.ball = Ball((Config.SCREEN_WIDTH // 2, Config.SCREEN_HEIGHT // 2))
 
@@ -46,20 +47,18 @@ class Environment:
             if player.rectangle.colliderect(self.ball.rectangle):
                 # Rewards AI algorithm for successfully bouncing the ball
                 player.score += Config.PLAYER_SCORE_FOR_BOUNCE
-                return (self.ball.rectangle.centery - player.rectangle.centery) / (Config.PLAYER_SIZE[1] // 2)
+                # TODO: Better system for determining angle of bounce
+                return (self.ball.rectangle.centery - player.rectangle.centery) / (Config.PLAYER_SIZE[1])
 
     @staticmethod
-    def account_for_y_collision(game_object):
-        """
-        Prevents game_object from going out-of-bounds in Y-axis by correcting its position.
-        :param game_object: Object of type GameObject.
-        """
+    def collision_with_lower_bound(game_object, change):
         min_y = 0
+        return game_object.rectangle.top + change <= min_y
+
+    @staticmethod
+    def collision_with_upper_bound(game_object, change):
         max_y = game_display.get_height()
-        if game_object.rectangle.bottom >= max_y:
-            game_object.rectangle.bottom = max_y - 1
-        if game_object.rectangle.top <= min_y:
-            game_object.rectangle.top = min_y + 1
+        return game_object.rectangle.bottom + change >= max_y
 
     def get_keys(self):
         keys = pygame.key.get_pressed()
@@ -68,21 +67,22 @@ class Environment:
         if keys[KeyBinds.UP_PLAYER_2] or keys[KeyBinds.DOWN_PLAYER_2]:
             self.update_player(self.players[1], [keys[KeyBinds.UP_PLAYER_2], keys[KeyBinds.DOWN_PLAYER_2]])
 
-    @staticmethod
-    def update_player(player, keys):
-        if keys[0] >= 0.5:
-            player.rectangle.centery -= Config.PLAYER_SPEED
-        elif keys[1] >= 0.5:
-            player.rectangle.centery += Config.PLAYER_SPEED
+    def update_player(self, player, keys):
+        change = 0
+        if keys[0] >= 0.5 and not self.collision_with_lower_bound(player, -Config.PLAYER_SPEED):
+            change = -Config.PLAYER_SPEED
+        elif keys[1] >= 0.5 and not self.collision_with_upper_bound(player, Config.PLAYER_SPEED):
+            change = Config.PLAYER_SPEED
+        player.rectangle.centery += change
 
     def update_ball(self):
+        if self.collision_with_upper_bound(self.ball, self.ball.velocity.y) or self.collision_with_lower_bound(self.ball, self.ball.velocity.y):
+            self.ball.velocity.y = -self.ball.velocity.y
+        self.ball.rectangle.center += self.ball.velocity
         collision = self.account_for_collision()
         if collision is not None:
             self.ball.velocity.x = -self.ball.velocity.x
             self.ball.velocity.y = collision * 10 + self.ball.velocity.y
-        self.ball.rectangle.center += self.ball.velocity
-        if self.ball.rectangle.bottom >= Config.SCREEN_HEIGHT or self.ball.rectangle.top <= 0:
-            self.ball.velocity.y = -self.ball.velocity.y
 
     def render_environment(self):
         game_display.fill((0, 0, 0))
