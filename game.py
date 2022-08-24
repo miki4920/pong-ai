793fbf5302dd490565971c3cb1856b1a154fcede
miki4920 <miki4920@gmail.com>
2022-08-24 18:14:08 +0100

    Rewrote collision system to prevent errors upon bouncing

--- game.py
+++ game.py
@@ -42,23 +42,28 @@ class Environment:
                         Player((Config.SCREEN_WIDTH, Config.SCREEN_HEIGHT // 2))]
         self.ball = Ball((Config.SCREEN_WIDTH // 2, Config.SCREEN_HEIGHT // 2))
 
-    def account_for_collision(self):
+    def collision_between_players_and_ball(self):
         for player in self.players:
             if player.rectangle.colliderect(self.ball.rectangle):
-                # Rewards AI algorithm for successfully bouncing the ball
                 player.score += Config.PLAYER_SCORE_FOR_BOUNCE
                 # TODO: Better system for determining angle of bounce
                 return (self.ball.rectangle.centery - player.rectangle.centery) / (Config.PLAYER_SIZE[1])
 
     @staticmethod
-    def collision_with_lower_bound(game_object, change):
+    def collision_with_top(game_object, change):
         min_y = 0
-        return game_object.rectangle.top + change <= min_y
+        if game_object.rectangle.top + change < min_y:
+            game_object.rectangle.top = min_y
+            return True
+        return False
 
     @staticmethod
-    def collision_with_upper_bound(game_object, change):
+    def collision_with_bottom(game_object, change):
         max_y = game_display.get_height()
-        return game_object.rectangle.bottom + change >= max_y
+        if game_object.rectangle.bottom + change > max_y:
+            game_object.rectangle.bottom = max_y
+            return True
+        return False
 
     def get_keys(self):
         keys = pygame.key.get_pressed()
@@ -69,20 +74,25 @@ class Environment:
 
     def update_player(self, player, keys):
         change = 0
-        if keys[0] >= 0.5 and not self.collision_with_lower_bound(player, -Config.PLAYER_SPEED):
-            change = -Config.PLAYER_SPEED
-        elif keys[1] >= 0.5 and not self.collision_with_upper_bound(player, Config.PLAYER_SPEED):
-            change = Config.PLAYER_SPEED
+        if keys[0] >= 0.5:
+            if not self.collision_with_top(player, -Config.PLAYER_SPEED):
+                change = -Config.PLAYER_SPEED
+        if keys[1] >= 0.5:
+            if not self.collision_with_bottom(player, Config.PLAYER_SPEED):
+                change = Config.PLAYER_SPEED
         player.rectangle.centery += change
 
     def update_ball(self):
-        if self.collision_with_upper_bound(self.ball, self.ball.velocity.y) or self.collision_with_lower_bound(self.ball, self.ball.velocity.y):
-            self.ball.velocity.y = -self.ball.velocity.y
-        self.ball.rectangle.center += self.ball.velocity
-        collision = self.account_for_collision()
+        collision = self.collision_between_players_and_ball()
         if collision is not None:
             self.ball.velocity.x = -self.ball.velocity.x
             self.ball.velocity.y = collision * 10 + self.ball.velocity.y
+        if self.ball.velocity.y > 0 and self.collision_with_bottom(self.ball, self.ball.velocity.y):
+            self.ball.velocity.y = -self.ball.velocity.y
+        elif self.ball.velocity.y < 0 and self.collision_with_top(self.ball, self.ball.velocity.y):
+            self.ball.velocity.y = -self.ball.velocity.y
+        else:
+            self.ball.rectangle.center += self.ball.velocity
 
     def render_environment(self):
         game_display.fill((0, 0, 0))
